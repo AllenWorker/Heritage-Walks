2f5da27506d806c5410167d13da01d1f70589d9e
GT <v244682@tafe.wa.edu.au>
2019-04-08 15:42:11 +0800

    updated welcome page
    Added little more information

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -7,4 +7,16 @@
     <div class="callout small-12 medium-12 large-12 text-center">
         <h2>Hello Admin</h2>
     </div>
+    <div class="grid-x grid-margin-x">
+        <div class=" callout small-12 medium-6 large-6 cell">
+            <h3>How to Use</h3>
+            <span>If you are new to this program it is recommended you read this</span>
+            <p> This program allows the user to add trails and stops to then be displayed in a mobile application.
+                To start off in the top bar there are two main links, "Trails" and "Stops".
+            These will allow you to look at the data concerning them. </p>
+        </div>
+        <div class="callout small-12 medium-6 large-6 cell">
+            <h3>Other information</h3>
+        </div>
+    </div>
 @endsection
