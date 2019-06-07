da09f86eef05d268a54adf202e532bb8d237a155
GT <v244682@tafe.wa.edu.au>
2019-06-07 08:50:09 +0800

    added username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,6 +20,10 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
+    public function username()
+    {
+        return 'username'; //or return the field which you want to use.
+    }
     /**
      * Where to redirect users after login.
      *
@@ -36,4 +40,6 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
 }
