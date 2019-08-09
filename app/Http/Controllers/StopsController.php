f3a8ddb7f6410019ca947efe35bc0bfefe2f82c1
Jon-Luke Willemsen <36464563+jonlukewtafe@users.noreply.github.com>
2019-08-09 15:21:18 +0800

    Modified Paginate; Still trying to solve issue with short description
    Modified Paginate; Still trying to solve issue with short description. If the description contains no spaces or line breaks it doesn't display correctly.

--- app/Http/Controllers/StopsController.php
+++ app/Http/Controllers/StopsController.php
@@ -15,7 +15,7 @@ class StopsController extends Controller
      */
     public function index()
     {
-        $stops = Stop::paginate(15);
+        $stops = Stop::paginate(4);
         return view(
             'stops.index', /* the view to see */
             compact('stops') /* send the $stops */
--- resources/views/stops/index.blade.php
+++ resources/views/stops/index.blade.php
@@ -10,7 +10,7 @@
             <a href="{{action('StopsController@create')}}" class="button success">Add Stop</a>
             <div class="grid-container fluid">
                 <div class="grid-x">
-            @foreach($stops as $aStop)
+                    @foreach($stops as $aStop)
 
                         <div class="col small-12 medium-6 large-6">
                             <div class="card">
@@ -26,33 +26,21 @@
                                 </div>
                                 <div class="card-section">
                                     <div class="grid-x">
-                                        <a class="col small-6" href="/stops/{{ $aStop->id }}">
+                                        <a class="col small-12" href="/stops/{{ $aStop->id }}">
                                             <img src="/images/stops/{{ $aStop->img }}">
                                         </a>
-                                        <div class="col small-6">
-                                            <p class="text-wrap">
+                                        <div class="col small-12">
+                                            <p class="large-text-center">
                                                 {{$aStop -> short_desc}}
                                             </p>
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
-
-
-                        {{--<tr>
-                            <td><p>{{ $aStop->id }}</p></td>
-                            <td><p>{{ $aStop->name }}</p></td>
-                            <td><img src="/images/stops/{{ $aStop->img }}" style="width:50px; height:50px; float:left;"></td>
-                            <td><p>{{ $aStop->street_location }}</p></td>
-                            <td><p>{{ $aStop->short_desc }}</p></td>
-                            <td><a href="/stops/{{ $aStop->id }}">Details</a></td>
-                        </tr>--}}
-            @endforeach
-
-            {{ $stops->links()}}
+                    @endforeach
                 </div>
+                {{ $stops->links()}}
             </div>
         </div>
     </div>
